ac811118803dd39e80cbe06eae62c1dabe09026f
Andy Hang <hang.andy.t@gmail.com>
2018-07-24 10:44:04 -0500

    added ci.js, added travis-ci, updated links to have http

--- AdvancedNodeBlog/config/ci.js
+++ AdvancedNodeBlog/config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://ahang:password123@ds139921.mlab.com:39921/blog-dev1',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
